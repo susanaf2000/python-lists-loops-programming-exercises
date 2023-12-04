7a31750ee752011adff8d29fd3d2f2666fc034f6
Susana Florêncio <101977568+susanaf2000@users.noreply.github.com>
2023-12-04 21:31:30 +0000

    changes1

--- exercises/12.5-Yes_and_no/app.py
+++ exercises/12.5-Yes_and_no/app.py
@@ -1,9 +1,9 @@
 theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
 
 def wiki_woko(x):
-    return "wiki" if x == 1 else "woko"
+return "wiki" if x == 1 else "woko"
 
-new_list = list(map(wiki_woko(x), theBools))
+new_list = list(map(lambda x: wiki_woko(x), theBools))
 print(new_list)
     
 
